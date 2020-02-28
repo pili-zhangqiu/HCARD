953b24a4992aa51dd99d687fed14fd0c596872e0
Pilar Zhang Qiu <32870045+pz716@users.noreply.github.com>
2020-02-28 07:26:55 +0000

    Update plot_2d.py

--- Data_Collection_Sup/plot_2d.py
+++ Data_Collection_Sup/plot_2d.py
@@ -9,47 +9,50 @@ import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 
 
-filename = "180Sides_1.tsv"
-#filename = "180Top_1.tsv"
-#filename = "360Sides_1.tsv"
-#ilename = "360Top_1.tsv"
-data = np.loadtxt(filename)
+filename_180S = "180Sides_1.tsv"
+filename_180T = "180Top_1.tsv"
+filename_360S = "360Sides_1.tsv"
+filename_360T = "360Top_1.tsv"
+
+data_180S = np.loadtxt(filename_180S)
+data_360S = np.loadtxt(filename_360S)
+data_180T = np.loadtxt(filename_180T)
+data_360T = np.loadtxt(filename_360T)
+
+data = [data_180S, data_360S, data_180T, data_360T]
+
 #skip row: skiprows=1
 #select columns: usecols =[0,1,2]
 #tsv file: delimiter = "\\t"
 
-x = np.asarray(data[:,0]) #don't necessarily need np.asarray() function 
-y = np.asarray(data[:,1])
-z = np.asarray(data[:,2])
-t = np.arange(0.0, len(x))
+for i in range(4):
+    
+    x = np.asarray(data[i[:,0]])
+    y = np.asarray(data[i[:,1]])
+    z = np.asarray(data[i[:,2]])
+    t = np.arange(0.0, len(x))
 
+    ax1 = plt.subplot(311)
+    ax2 = plt.subplot(312)
+    ax3 = plt.subplot(313)
 
+    ax1.plot(t,x)
+    ax1.set_title('plot from file: '+filename+ ', data#' + str(len(x)+1))
+    #total #of data points
+    ax1.set_ylabel('x (roll)')
 
+    ax2.plot(t,y)
+    #ax2.set_ylim((-180, 180))
+    ax2.set_ylabel('y (pitch)')
 
-ax1 = plt.subplot(311)
-ax2 = plt.subplot(312)
-ax3 = plt.subplot(313)
+    ax3.plot(t,z)
+    ax3.set_ylabel('z (yaw)')
 
-ax1.plot(t,x)
-ax1.set_title('plot from file: '+filename+ ', data#' + str(len(x)+1))
-#total #of data points
-ax1.set_ylabel('x (roll)')
+    #plt.setp(ax1.get_xticklabels(), visible=False) #invisible x ticks for plot 1
 
-ax2.plot(t,y)
-#ax2.set_ylim((-180, 180))
-ax2.set_ylabel('y (pitch)')
+    plt.tight_layout()
 
-ax3.plot(t,z)
-ax3.set_ylabel('z (yaw)')
+    #plt.show()
 
-#plt.setp(ax1.get_xticklabels(), visible=False) #invisible x ticks for plot 1
-
-plt.tight_layout()
-
-#plt.show()
-
-for i in range(10):
-    ...................
-    ...................
     num = "%i.png" % i
     plt.savefig(num, format='png')
