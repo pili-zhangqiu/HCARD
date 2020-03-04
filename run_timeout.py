4b16e7479bcf3ec9e764d40b283328ec3750573d
pz716 <pz716@ic.ac.uk>
2020-03-04 02:59:43 +0000

    run_timeout repetitively

--- run_timeout.py
+++ run_timeout.py
@@ -1,54 +1,28 @@
 import os
 import subprocess
 import time
+import timeit
 import threading as thread
 
-raw_cmd = "minimu9-ahrs --mode raw > startup_test3.tsv"
+raw_cmd = "minimu9-ahrs --mode raw > start_test.tsv"
+
+def hello():
+  print(time.ctime()) #current time
+  thread.Timer(10.0, hello).start() # called every 10s
+  print("Hello, World!")
+  print("Recording... ", raw_cmd)
+  action = subprocess.Popen(raw_cmd, shell=True)
+  time.sleep(2)
 
 
-tic = time.perf_counter() #time counter
+hello()
+       
+
+#tic = time.perf_counter() #time counter
 # print(tic)
-print("Recording... ", raw_cmd)
-action = subprocess.Popen(raw_cmd, shell=True)
-action.wait(3)
-action.terminate()
-action.wait()
-toc = time.perf_counter()
-print(toc-tic)
-
-'''
-stop_time = time.time() + 10
-
-os.system("minimu9-ahrs --mode raw > output_supinate1.csv")
-
-try:
-    while time.time() < stop_time:
-        continue
-        
-except KeyboardInterrupt:
-    pass
-
-sys.stdout.write("Exiting")
-sys.exit(0)
-'''
-
-'''
-os.system("minimu9-ahrs --mode raw > output_supinate.csv")
-sys.stdout.write("Recording...")
-
-time.sleep(1)  # In seconds
-
-sys.stdout.write("Exiting")
-sys.exit(0)
-sys.stdout.write("Exited")
-'''
-
-'''
-def job():
-  os.system("minimu9-ahrs --mode raw > output_supinate.csv")
-  time.sleep(10)
-
-schedule.every(10).minutes.do(job)
-schedule.every().hour.do(job)
-schedule.every().day.at("10:30").do(job)
-'''
+#print("Recording... ", raw_cmd)
+#action = subprocess.Popen(raw_cmd, shell=True)
+#action.wait(10)
+#action.terminate()
+#toc = time.perf_counter()
+#print(toc-tic)
