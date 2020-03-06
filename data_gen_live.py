622b716b17a0704c217d96b31e81db74ef1d622e
zcoco99 <36939425+zcoco99@users.noreply.github.com>
2020-03-06 13:30:10 +0000

    Update data_gen_live.py

--- data_gen_live.py
+++ data_gen_live.py
@@ -1,21 +1,22 @@
 import csv
 import random
 import time
+from datetime import datetime
 
 x_value = 0
 total_1 = 1000
 total_2 = 1000
 
 fieldnames = ["x_value", "total_1", "total_2"]
+name = datetime.now().strftime('%Y-%m-%d-%H-%M.csv')
 
-
-with open('random_gen.csv', 'w') as csv_file:
+with open(name, 'w') as csv_file:
     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     csv_writer.writeheader()
 
 while True:
 
-    with open('random_gen.csv', 'a') as csv_file:
+    with open(name, 'a') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
         info = {
@@ -32,3 +33,4 @@ while True:
         total_2 = total_2 + random.randint(-5, 6)
 
     time.sleep(1)
+    
